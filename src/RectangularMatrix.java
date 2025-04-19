603f15e3f983a9bb5cefad996e9688e2cc4afcc4
Bindu Thotakura <132102851+String369@users.noreply.github.com>
2025-04-18 23:11:56 -0500

    Added row logic for rectangular matrix

--- src/RectangularMatrix.java
+++ src/RectangularMatrix.java
@@ -1,15 +1,14 @@
 public class RectangularMatrix {
     public static int solution(char[][] board, String word) {
-        int rows = board.length;
         int cols = board[0].length;
         int wlen = word.length();
         int count = 0;
 
         // Horizontal (rows)
-        for (int i = 0; i < rows; i++) {
+        for (char[] chars : board) {
             for (int j = 0; j <= cols - wlen; j++) {
                 int k = 0;
-                while (k < wlen && board[i][j + k] == word.charAt(k)) {
+                while (k < wlen && chars[j + k] == word.charAt(k)) {
                     k++;
                 }
                 if (k == wlen) count++;
