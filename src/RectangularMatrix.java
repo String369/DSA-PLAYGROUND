84752da531359e14aa394eb4f861b043197a6168
Bindu Thotakura <132102851+String369@users.noreply.github.com>
2025-04-19 23:13:58 -0500

    Added column logic for rectangular matrix

--- src/RectangularMatrix.java
+++ src/RectangularMatrix.java
@@ -1,14 +1,26 @@
 public class RectangularMatrix {
     public static int solution(char[][] board, String word) {
+        int rows = board.length;
         int cols = board[0].length;
         int wlen = word.length();
         int count = 0;
 
         // Horizontal (rows)
-        for (char[] chars : board) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j <= cols - wlen; j++) {
                 int k = 0;
-                while (k < wlen && chars[j + k] == word.charAt(k)) {
+                while (k < wlen && board[i][j + k] == word.charAt(k)) {
+                    k++;
+                }
+                if (k == wlen) count++;
+            }
+        }
+
+        // Vertical (columns)
+        for (int j = 0; j < cols; j++) {
+            for (int i = 0; i <= rows - wlen; i++) {
+                int k = 0;
+                while (k < wlen && board[i + k][j] == word.charAt(k)) {
                     k++;
                 }
                 if (k == wlen) count++;
@@ -16,7 +28,6 @@ public class RectangularMatrix {
         }
         return count;
     }
-
 public static void main(String[] args) {
     char[][] board = {
             {'a', 'a'},
