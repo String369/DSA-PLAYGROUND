a844cebd4c63fbe57bfe6882ea5d8a72b18b9ecf
Bindu Thotakura <132102851+String369@users.noreply.github.com>
2025-04-30 16:01:25 -0500

    Solved Array: PlusOne easy problem

---
+++ src/PlusOne.java
@@ -0,0 +1,27 @@
+public class PlusOne {
+    public int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for(int i=n-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+
+            digits[i] = 0;
+        }
+
+        int[] result = new int[n+1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PlusOne po = new PlusOne();
+        int[] digits = {9, 9, 9};
+        int[] result = po.plusOne(digits);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+}
